bef93e38669973fde8b81ae81e6d7690b50edc72
p1yslee <p1yslee@gmail.com>
2021-03-01 11:28:29 +0900

    137,140 풀이 방법 해설 추가

---
+++ ep51to100/54.py
@@ -0,0 +1,6 @@
+poker_hierhachy = {0 : 'High Card',1 : 'One Pair', \
+                   2 : 'Two Pair', 3 : 'Three of a Kind', \
+                   4 : 'Straight', 5 : 'Flush', \
+                   6 : 'Full House', 7 : 'Four of a Kind', \
+                   8 : 'Straight Flush', 9 : 'Royal Flush'\
+                   }
