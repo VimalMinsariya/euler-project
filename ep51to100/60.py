c874cb1058e8cdf259b68c32b57db9c0f83a30af
p1yslee <p1yslee@gmail.com>
2021-03-15 15:20:40 +0900

    60번 문제 풀이. 그래프 이론. http://arxiv.org/pdf/0805.1449.pdf, miller-rabin test에 대해 알게 됨.

--- ep51to100/60.py
+++ ep51to100/60.py
@@ -1,26 +1,46 @@
-primes = [2,3,5,7,9]
-test = []
-pset = dict()
+def sumOfDigit(n):
+    s, q = 0, n
+    while q > 0:
+        q, r = divmod(q,10)
+        s += r
+    return s
 
-def limit(n):
-    return int(n**(1/2)) + 1
+def isPrime(n):
+    limit = (n+1)**(1/2)
+    if n == 1: return False
+    if n == 2: return True
+    if n%2 == 0: return False
+    else:
+        k = 3
+        while k < limit:
+            if n % k == 0: return False
+            k += 2
+    return True
 
-n = 11
-while p in list(filter(lambda prime: prime < limit(n), primes)):
-    if n % p == 0:
-        isPrime = False
-        break
-if isPrime == True:
-    if len(str(n)) > len(str(primes[-1])):
-        for p in test:
-            k = str(p)
-            for cut in range(1,len(k)):
-                a, b = k[:cut], k[cut:]
-                a, b, c = int(a), int(b), int(b+a)
-                if a in primes and b in primes and c in primes:
-                    if pset
-        test = []
-    primes.append(n)
-    test.append(p)
+def isPairPrime(p,q):
+    a = int(str(p)+str(q))
+    if sumOfDigit(a) % 3 == 0: return False
+    b = int(str(q)+str(p))
+    result = isPrime(a) and isPrime(b)
+    return result
 
-n += 2
+def isMember(u,q):
+    for p in u:
+        if not isPairPrime(p,q): return False
+    return True
+
+series = [[3]]
+q = 7
+test = True
+while test:
+    if isPrime(q):
+        for u in series:
+            if isMember(u,q):
+                series.append(u+[q])
+            if len(series[-1]) == 5:
+                test = False
+                break
+        series.append([q])
+    q += 2
+
+print(series[-2], sum(series[-2]))
