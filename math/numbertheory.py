9f767aa289c44b9b2f4e0287d911ecaed57a9609
p1yslee <p1yslee@gmail.com>
2021-03-19 15:28:25 +0900

    에라토스테네스 체 추가

--- math/numbertheory.py
+++ math/numbertheory.py
@@ -132,3 +132,18 @@ def lys_root(m,n:int ,l:int) -> str:
         r = 10 * r + s
         k += 1
     return result
+
+# 에라토스테네스 체
+def esieve(n):
+    li = [0,0] + [i for i in range(2,n+1)]
+    sieve = []
+    k = 0
+    while k < n+1:
+        if li[k] != 0:
+            sieve.append(li[k])
+            p = 2*k
+            while p < n+1:
+                li[p] = 0
+                p += k
+        k += 1
+    return sieve
