510c9d98b2fb7d05f90091f83d988d286a7bb166
p1yslee <p1yslee@gmail.com>
2021-04-18 12:57:38 +0900

    187번 풀. 에라토스테네스 체를 잘 구현했음.

---
+++ 100/187_Semiprimes.py
@@ -0,0 +1,26 @@
+import math
+
+def prime_seive(n):
+    num = {i:0 for i in range(2,n)}
+    limit = int(n**(1/2))
+    for k in range(2,limit+1):
+        if num[k] == 0:
+            for j in range(2*k,n,k):
+                num[j] = 1
+    return list(filter(lambda x: num[x]==0, num))
+
+N =10**8
+primes = prime_seive(N)
+l = len(primes)
+print('ok',l)
+tail = l-1
+cnt = 0
+for start in range(l):
+    if tail < start : break
+    h = primes[start]
+    while h*primes[tail] >= N:
+        tail -= 1
+    cnt += tail - start + 1
+    k = math.log10(start+1)
+    if k == int(k): print(start+1)
+print(cnt)
